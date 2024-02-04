a6484bc727f498277656ac34eebc243e44520e9f
Ryan Singleton <ryansingleton@Ryans-MacBook-Pro.local>
2024-02-03 21:18:41 -0500

    cors adjustment

--- week3-4Project/routes/users.js
+++ week3-4Project/routes/users.js
@@ -1,19 +1,18 @@
-const cors = require("cors");
 const routes = require("express").Router();
 const usersController = require("../controllers/users");
 const { isAuthenticated } = require("../middleware/authenticate");
 
-routes.get("/", cors(), usersController.getAllUsers);
-routes.get("/:user_id", cors(), usersController.getOneUser);
+routes.get("/", usersController.getAllUsers);
+routes.get("/:user_id", usersController.getOneUser);
 
 // create a user
-routes.post("/", cors(), isAuthenticated, usersController.create);
+routes.post("/", isAuthenticated, usersController.create);
 
 // update a user
-routes.put("/:_id", cors(), isAuthenticated, usersController.update);
+routes.put("/:_id", isAuthenticated, usersController.update);
 
 // delete a user or all users
-routes.delete("/:_id", cors(), isAuthenticated, usersController.deleteOne);
-routes.delete("/", cors(), isAuthenticated, usersController.deleteAll);
+routes.delete("/:_id", isAuthenticated, usersController.deleteOne);
+routes.delete("/", isAuthenticated, usersController.deleteAll);
 
 module.exports = routes;
--- week3-4Project/server.js
+++ week3-4Project/server.js
@@ -18,12 +18,6 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
