9d217afc1343846d0378237343e9d620beb47c76
Ryan Singleton <ryansingleton@Ryans-MacBook-Pro.local>
2024-02-03 20:21:13 -0500

    fixed url again

--- week3-4Project/swagger.js
+++ week3-4Project/swagger.js
@@ -1,17 +1,16 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
-console.log('process.env.PORT', process.env.PORT);
-const host = process.env.PORT == 8080? 'localhost:8080' : 'https://reviews-ya3k.onrender.com';
 const doc = {
   info: {
-    title: 'Reviews and Users API',
-    description: 'This API allows you to create, read, update, and delete reviews and users.'
+    title: "Reviews and Users API",
+    description:
+      "This API allows you to create, read, update, and delete reviews and users.",
   },
-  host: host,
+  host: "reviews-ya3k.onrender.com",
 };
 
-const outputFile = './swagger.json';
-const routes = ['./routes/index.js'];
+const outputFile = "./swagger.json";
+const routes = ["./routes/index.js"];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
